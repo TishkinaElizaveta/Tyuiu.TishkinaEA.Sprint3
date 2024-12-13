0051a85301038c64dddc49a5824d29a35e1f0088
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-14 00:01:59 +0500

    Task1

---
+++ Tyuiu.TishkinaEA.Sprint3.Task1.V11.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.TishkinaEA.Sprint3.Task1.V11.Lib
+{
+    public class DataService : ISprint3Task1V11
+    {
+        public double GetMultiplySeries(double value, int startValue, int stopValue)
+        {
+            double product = 1; 
+            int k = startValue;  
+
+            while (k <= stopValue)
+            {
+                double term = Math.Pow(value, k) + 0.5; 
+                product *= term; 
+                k++; 
+            }
+
+            return product; 
+        }
+    }
+}
---
+++ Tyuiu.TishkinaEA.Sprint3.Task1.V11.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.TishkinaEA.Sprint3.Task1.V11.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
