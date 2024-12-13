b7efc4d19bbd755ce970224753fed08c558ec42e
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-14 00:40:05 +0500

    Task6

---
+++ Tyuiu.TishkinaEA.Sprint3.Task6.V14.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.TishkinaEA.Sprint3.Task6.V14.Lib
+{
+    public class DataService : ISprint3Task6V14
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int totalDivisors = 0; 
+
+            for (int number = startValue; number <= stopValue; number++)
+            {
+                for (int divisor = 6; divisor <= number; divisor++) 
+                {
+                    if (number % divisor == 0) 
+                    {
+                        totalDivisors++; 
+                    }
+                }
+            }
+
+            return totalDivisors; 
+        }
+    }
+}
